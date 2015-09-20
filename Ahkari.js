cb8efa7cae1a097c0dd205886681fa270b95b6f5
Ahkari <gqz825584053@qq,com>
2015-09-20 20:48:31 +0800

    Ahkaris story.

---
+++ Ahkari.js
@@ -0,0 +1,58 @@
+//喜欢的妹子是web测试
+function girl(){
+	this.job = 'webTester' ;
+	this.name = 'lily' ;
+}
+//所以从测试转为web前端
+function boy(){
+	this.job = 'webDeveloper' ;
+	this.name = 'ahkari' ;
+}
+//努力的学着一切
+ahkari.proptype = {
+	seajs : function(){
+	
+	},
+	jquery : function(){
+
+	},
+	javascript : function(){
+
+	}
+}
+//也证明了自己, 转岗后几个月就跳槽了, 在二线城市拿着相对不错的薪资
+var Ahkari = new boy() ;
+Ahkari.company = a_game_company ;
+//现在的公司用的是git版本管理
+function intenetCompany(){
+
+}
+var a_game_company = new intenetCompany() ;
+a_game_company.resourse_control = 'git' ;
+//于是我重拾几个月前开始但还没看完的本教程
+Ahkari.prototype.git = function(){
+
+}
+//我一步步敲出了所有命令, 现在终于完全弄懂了git, 甚至之前半知半解的svn也豁然开朗
+Ahkari.prototype.svn = function(){
+
+}
+//谢谢, 现在虽然和妹子没什么机会了.....
+var Lily = new girl() ;
+/*
+Lily.boyfriend = 'Ahkari' ;
+*/
+//但是还是感谢她能让我找到自己的路.
+Ahkari.career_planning = 'javascript Developer' ;
+//谢谢你, 芹. 
+Ahkari.prototype.sayThanks = function(){
+	// console.log('Thank you, Qin') ;
+	alert('Thank you ! Qin') ;
+}
+//如果最后不能和你在一起, 还是感谢你所给我的无心插柳的一切. 
+if ( !!Lily.boyfriend && Lily.boyfriend!=='Ahkari' ){
+	alert('Thank you for everythings you give me') ;
+}else{
+	console.log('I will love you until end.') ;
+	console.log('Thank you my dear.') ;
+}
