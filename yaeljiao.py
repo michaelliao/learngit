b5b65a89a12d84c5789b1f3dce40f0222fb0fd5f
Datou5680 <yaeljiao@gmail.com>
2017-01-15 19:29:22 -0600

    Share my feeling on programming (#1799)

---
+++ yaeljiao.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Zhichao'
+
+print('''
+     I'm learning Git & Python on http://www.liaoxuefeng.com/
+     Programming makes me happy. Thanks to master Liao! :)
+     ''')
